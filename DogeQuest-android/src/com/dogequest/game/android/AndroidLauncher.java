b6f58ea8c85ac84b7e8cd570ef2e6ef28a2ff1df
macej <matej.tabi@gmail.com>
2014-07-17 11:29:29 +0200

    pridane aj android a desktop casti

---
+++ DogeQuest-android/src/com/dogequest/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.dogequest.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.dogequest.game.DogeQuest;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new DogeQuest(), config);
+	}
+}
---
+++ DogeQuest-desktop/src/com/dogequest/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dogequest.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dogequest.game.DogeQuest;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DogeQuest(), config);
+	}
+}
